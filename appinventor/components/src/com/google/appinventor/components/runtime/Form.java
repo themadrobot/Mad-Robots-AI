272b4b8f11c9ba9bbf018e0bd7a6c04290119ba8
App Inventor <gareth.haylings@googlemail.com>
2014-06-17 10:24:26 +0100

    Fix to prevent the user removed yandexTranslateTagline

--- appinventor/components/src/com/google/appinventor/components/runtime/Form.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Form.java
@@ -1359,7 +1359,7 @@ public class Form extends Activity
   private void showAboutApplicationNotification() {
     String title = "About This App";
     String tagline = "<p><small><em>Invented with MIT App Inventor<br>appinventor.mit.edu</em></small>";
-    String message = aboutScreen + tagline + yandexTranslateTagline;
+    String message = tagline + yandexTranslateTagline + aboutScreen;
     String buttonText ="Got it";
     Notifier.oneButtonAlert(this, message, title, buttonText);
   }
